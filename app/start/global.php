99a75860059247fd67d646a1e64dc6d3a9300734
Lance Padiernos <jomarilance2@gmail.com>
2015-11-21 08:03:25 +0800

    try3

--- app/start/global.php
+++ app/start/global.php
@@ -49,7 +49,7 @@ Log::useFiles(storage_path().'/logs/laravel.log');
 App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
-	return "<h1>Page Not Found</h1>";
+	//return "<h1>Page Not Found</h1>";
 });
 
 /*
